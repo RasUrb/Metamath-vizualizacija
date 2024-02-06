8abd9865e722d0b44de0678a289d0d71f34630b8
RasUrb <82056577+RasUrb@users.noreply.github.com>
2024-02-06 08:36:01 +0200

    Add files via upload

---
+++ negTrue.mm
@@ -0,0 +1,14 @@
+$( irodymas kad |- -.x -> x $)
+
+$c -. -> |- wff ( ) $. $( zenkldai $)
+$v x $. $( simboliai $)
+
+wx $f wff x $.
+
+wxn $e |- ( -. -. x -> x ) $.
+wxnn $e |- -. x $.
+wxnp $a |- x $.
+
+proof $p |- x $= wx wxn wxnn wxnp $.
+
+
---
+++ test2.mm
@@ -0,0 +1,12 @@
+$(  (A & B) -> A $)
+
+$c -> & |- ( ) wff $.
+$v A B $.
+
+wa $f wff A $.
+wb $f wff B $.
+
+wawb $e |- (A & B) $.
+wbwa $a |- (B & A) $.
+
+proof $p |- (B & A) $= wa wb wawb wbwa $.
